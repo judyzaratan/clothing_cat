f10dda8d6259ed0a7abe07c2f4dddc4905932291
mochicat8 <mochicat8@gmail.com>
2017-07-26 15:26:18 -0700

    pep8 fixes

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+from flask import Flask, render_template, url_for, request, redirect,\
+    flash, jsonify
 
 
 from sqlalchemy import create_engine
@@ -22,37 +23,45 @@ def catalogCategories():
     items = session.query(Item)
     return render_template('catalog.html', catalog=catalog)
 
+
 @app.route('/category/<int:category_id>/', methods=['GET', 'POST'])
 def categoryItems(category_id):
     if request.method == 'GET':
         categories = session.query(Category).all()
         items = session.query(Item).filter_by(category_id=category_id)
         if items.count():
-            return render_template('category_items.html', items=items, categories=categories)
+            return render_template('category_items.html',
+                                   items=items,
+                                   categories=categories)
         else:
             return render_template('catalog.html', categories=categories)
 
-@app.route('/category/<int:category_id>/edit/<int:item_id>/', methods=['GET', 'POST'])
+
+@app.route('/category/<int:category_id>/edit/<int:item_id>/',
+           methods=['GET', 'POST'])
 def editItem(category_id, item_id):
     item_query = session.query(Item).filter_by(id=item_id).one()
     prev_name = item_query.name
     if request.method == 'GET':
-        return render_template('editItem.html', item=item_query, category_id=category_id, item_id=item_id)
+        return render_template('editItem.html',
+                               item=item_query,
+                               category_id=category_id,
+                               item_id=item_id)
     if request.method == 'POST':
         if request.form['name']:
             item_query.name = request.form['name']
         if request.form['description']:
             item_query.description = request.form['description']
         session.commit()
-        return redirect(url_for('categoryItems',category_id=category_id))
+        return redirect(url_for('categoryItems', category_id=category_id))
 
 
 @app.route('/catalog/add/', methods=['GET', 'POST'])
 def addItem():
     if request.method == 'POST':
         newItem = Item(name=request.form['name'],
-                           description=request.form['description'],
-                           category_id=int(request.form['category_id']))
+                       description=request.form['description'],
+                       category_id=int(request.form['category_id']))
         session.add(newItem)
         session.commit()
         return redirect(url_for('catalogCategories'))
@@ -60,7 +69,8 @@ def addItem():
         categories = session.query(Category)
         return render_template('addItem.html', categories=categories)
 
-@app.route('/catalog/delete/<int:item_id>', methods=['GET','POST'])
+
+@app.route('/catalog/delete/<int:item_id>', methods=['GET', 'POST'])
 def deleteItem(item_id):
     if request.method == 'GET':
         item_to_delete = session.query(Item).filter_by(id=item_id).one()
@@ -70,10 +80,10 @@ def deleteItem(item_id):
         session.delete(item_to_delete)
         return redirect(url_for('catalogCategories'))
 
+
 # If not used as an imported module run following code
 if __name__ == '__main__':
     # Server reload on code changes
     app.secret_key = 'super_secret_key'
     app.debug = True
-    #
     app.run(host='0.0.0.0', port=5000)
