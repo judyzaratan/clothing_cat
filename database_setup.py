50ca4304c41a4064e3300b2e6ae27a15884b18f1
mochicat8 <mochicat8@gmail.com>
2017-07-26 15:39:47 -0700

    pep8 fixes across board

--- database_setup.py
+++ database_setup.py
@@ -7,31 +7,34 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(32), index=True)
+
 
 class Category(Base):
     __tablename__ = 'category'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
+
 
 class Item(Base):
     __tablename__ = 'item'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-
 
     @property
     def serialize(self):
         return {
             'name': self.name,
             'description': self.description,
-            'id': self.id
+            'id': self.id,
+            'category': self.category.name
         }
 
 
--- project.py
+++ project.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect,\
     flash, jsonify
 
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
